46565933b3760a258f2f8722a278c754c1152a98
BCsabaA <85181780+BCsabaA@users.noreply.github.com>
2023-09-09 06:13:46 +0200

    Initial commit

---
+++ transport-notes/src/main/java/com/barab/transportnotes/TransportNotesApplication.java
@@ -0,0 +1,13 @@
+package com.barab.transportnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportNotesApplication.class, args);
+	}
+
+}
