500c444e3e769ea8bfc4a2f6f4f5d773de7fc5c6
BCsabaA <85181780+BCsabaA@users.noreply.github.com>
2023-09-14 05:55:01 +0200

    index.html finished, register.html started

--- transport-notes/src/main/java/com/barab/transportnotes/controller/AuthController.java
+++ transport-notes/src/main/java/com/barab/transportnotes/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.barab.transportnotes.controller;
 
+import com.barab.transportnotes.dto.UserDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,6 +12,16 @@ public class AuthController {
     @GetMapping("/index")
     public String home() {
         return"index";
+    }
+
+    //handler method to handle user registration form request
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        //create a model object to store form data
+        UserDto user = new UserDto();
+        model.addAttribute("user", user);
+        return "register";
+        //TODO: register.html folytatása navbar hozzádásával (259-es lecke)
     }
 
 }
