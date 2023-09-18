996e1741020c449dd6caa773148eb40e29cf9fab
BCsabaA <85181780+BCsabaA@users.noreply.github.com>
2023-09-18 06:10:36 +0200

    users list backend done

--- transport-notes/src/main/java/com/barab/transportnotes/service/UserService.java
+++ transport-notes/src/main/java/com/barab/transportnotes/service/UserService.java
@@ -1,7 +1,13 @@
 package com.barab.transportnotes.service;
 
 import com.barab.transportnotes.dto.UserDto;
+import com.barab.transportnotes.entity.User;
+
+import java.util.List;
 
 public interface UserService {
     public void saveUser(UserDto userDto);
+
+    User findUserByEmail(String email);
+    List<UserDto> findAllUsers();
 }
