3053a1fa9bd8033dfabfc1c719a07ab599093faa
BCsabaA <85181780+BCsabaA@users.noreply.github.com>
2023-10-01 07:24:46 +0200

    address selection change
    added js funcionality for chaneg of address on new note form, both for source and destination address

--- transport-notes/src/main/resources/static/js/script.js
+++ transport-notes/src/main/resources/static/js/script.js
@@ -1,5 +1,24 @@
-function setSourceAddressDetails() {
-   var textSourceAddress = document.getElementById("sourceAddress");
-   textSourceAddress.value = "cím";
+function setAddressDetails(groupIdentifier) {
+   var textAddress = document.getElementById(groupIdentifier + "Address");
+   var textComment = document.getElementById(groupIdentifier + "Comment");
+   var addressName = document.getElementById(groupIdentifier + "NameDropdown").value;
+
+   addressesArray = [...addresses];
+   console.log(addressesArray);
+
+
+   console.log(addressName);
+
+   addressesArray.forEach(element => {
+      console.log(addressesArray);
+      if (element.name === addressName) {
+         console.log("element found;");
+         console.log(element);
+            textAddress.value = element.address;
+            textComment.value = element.comment;
+      }
+   });
+
+
 }
 
